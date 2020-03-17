cbc1748e9fe98a37430a502e03d7fdbe2e6a468f
Kamille Longalong <k.pagibigan@gmail.com>
2020-03-17 08:03:23 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,7 +6,7 @@ class Student < User
     @knowledge_array = []
   end
   
-  def learn(knowledge) #takes in an argument of a string
+  def learn(knowledge) #takes in an argument of a
     @knowledge_array << knowledge
   end
   
