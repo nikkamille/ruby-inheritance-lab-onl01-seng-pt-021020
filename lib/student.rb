c298fb143c61e1e6a41945916261d66b3ff95d2c
Kamille Longalong <k.pagibigan@gmail.com>
2020-03-17 07:54:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,4 +2,16 @@ require_relative "./user.rb"
 
 class Student < User
 
+  def new
+    @knowledge_array = []
+  end
+  
+  def learn(knowledge)
+    @knowledge_array << knowledge
+  end
+  
+  def knowledge
+    
+  end
+
 end
